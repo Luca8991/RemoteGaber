fa7ae848b4a7e00b3bd4de930e0a97a9e751003b
Luca8991 <luca.berte98@hotmail.it>
2020-06-13 19:37:42 +0200

    logs on oled

--- esp8266/boot.py
+++ esp8266/boot.py
@@ -1,7 +1,7 @@
 # This file is executed on every boot (including wake-boot from deepsleep)
 #import esp
 #esp.osdebug(None)
-import uos, machine
+import uos, machine, sh1106
 #uos.dupterm(None, 1) # disable REPL on UART(0)
 import gc
 #import webrepl
@@ -9,14 +9,32 @@ import gc
 gc.collect()
 
 import network
+
+i2c = machine.I2C(scl=machine.Pin(4), sda=machine.Pin(5))
+oled = sh1106.SH1106_I2C(128, 64, i2c, None, 0x3c)
+
+SSID = "<your-ssid>"
+PWD = "<your-pwd>"
+
+oled.fill(0)
+oled.text("connecting to wifi network:", 0, 0)
+oled.text(SSID, 0, 10)
+oled.rotate(True)
+oled.show()
+
 def do_connect():
     sta_if = network.WLAN(network.STA_IF)
     if not sta_if.isconnected():
         print('connecting to network...')
         sta_if.active(True)
-        sta_if.connect('Vodafone-33866776', 'bdacpslt2mtdjvu')
+        sta_if.connect(SSID, PWD)
         while not sta_if.isconnected():
             pass
+    oled.fill(0)
+    oled.text("connected!", 0, 0)
+    oled.text("IP:"+str(sta_if.ifconfig()[0]), 0 ,10)
+    oled.rotate(True)
+    oled.show()
     print('network config:', sta_if.ifconfig())
     
 do_connect()
